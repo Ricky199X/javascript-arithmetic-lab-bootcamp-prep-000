3257aca14f593bccb2e9b94a4569987d2302461b
Ricky Rojas <rickyrojas19@gmail.com>
2018-10-02 22:31:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 function add (a, b) {
   return a + b;
@@ -32,3 +33,30 @@ function preserveDecimal (n, base) {
   var parsed = parseFloat (n, 10);
   return parsed
 }
+=======
+var a = 10;
+var b = 20;
+
+
+function add (a, b) {
+  result = 10 + 20;
+  return result;
+}
+
+function subtract (a, b) {
+  
+}
+
+function multiply (a, b) {
+  
+}
+
+function divide (a, b) {
+  
+}
+
+
+
+
+
+>>>>>>> 4715205e1f01175290f95964fa084f5eaca803fe
